22b93f3b07fc071f1372536223230828ae5b5cfd
ratno putro sulistiyono <ratno@knoqdown.com>
2018-10-12 14:47:06 +0700

    tambahkan request dengan oauth2

---
+++ src/RequestOauth2.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ratno\Transport;
+
+class RequestOauth2
+{
+    protected $base_url;
+    protected $access_token;
+
+    function __construct($client_id, $client_secret, $base_url)
+    {
+        $this->base_url = $base_url;
+        $this->access_token = $this->authClient($client_id,$client_secret);
+    }
+
+    protected function authClient($client_id,$client_secret)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request("POST", $this->base_url . "/client_token.php", [
+            'form_params' => [
+                "grant_type" => "client_credentials",
+                "client_id" => $client_id,
+                "client_secret" => $client_secret
+            ]
+        ]);
+
+        $data = json_decode($response->getBody()->getContents(),true);
+        if(array_key_exists("tiketux",$data)
+            && array_key_exists("status",$data["tiketux"])
+            && $data["tiketux"]["status"] == "OK") {
+
+            return $data["tiketux"]["result"]["access_token"];
+        }
+
+        return "";
+    }
+
+    protected function call($method, $url, $request_params = [])
+    {
+        $client = new \GuzzleHttp\Client();
+
+        if($method == "GET") {
+            $result = $client->request($method, $url ."?". http_build_query($request_params),[
+                'headers' => ['Authorization' => 'Bearer ' . $this->access_token]
+            ]);
+        } else {
+            $result = $client->request($method, $url, [
+                'form_params' => $request_params,
+                'headers' => ['Authorization' => 'Bearer ' . $this->access_token]
+            ]);
+        }
+
+        return $result->getBody()->getContents();
+    }
+
+    public function get($endpoint_url,$request_params = [])
+    {
+        return $this->call("GET", $this->base_url . $endpoint_url, $request_params);
+    }
+
+    public function post($endpoint_url,$request_params = [])
+    {
+        return $this->call("POST", $this->base_url . $endpoint_url, $request_params);
+    }
+}
